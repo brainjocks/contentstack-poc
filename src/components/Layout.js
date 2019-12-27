77e9d5db366b56aa7f9eb044f81fb1f6ecc93c9f
Mike Baker <mikebaker@brainjocks.com>
2019-12-27 12:13:26 -0500

    Update img folder to include the images needed for css files

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 //import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import Footer from "./Footer"
+import Nav from "./Nav"
 
 //import Header from './header'
 import '../css/main.css'
@@ -21,23 +23,18 @@ const Layout = ({ children }) => (
       <div>
         {/* <Helmet title={data.site.siteMetadata.title} />
         <Header siteTitle={data.site.siteMetadata.title} /> */}
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <Nav/>
+        <div className='page-wrapper'>
           {children}
         </div>
+        <Footer/>
       </div>
     )}
   />
 )
 
 Layout.propTypes = {
-  children: PropTypes.object,
+  children: PropTypes.node,
 }
 
 export default Layout
